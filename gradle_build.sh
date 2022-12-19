e0598e7a5dc56f019876f81891e34592f0ad90c0
gavlaaaaaaaa <gavlaaaaa5@gmail.com>
2022-12-19 08:58:15 +0000

    added input

--- gradle_build.sh
+++ gradle_build.sh
@@ -1,11 +1,19 @@
 #!/bin/sh -l
 
-gradle build
+# run the build command
+gradle build $1
+
+# capture the exit code of the gradle command
+# if it fails then exit the script
 retval=$?
 if [ ${retval} -ne 0 ]
 then
     exit ${retval}
 fi
 
-fname=`ls $(pwd)/app/build/libs/` 
+# get the name of the jar file
+fname=`ls $(pwd)/app/build/libs/`
+
+# output the location variable into the output file location 
+# provided by the $GITHUB_OUTPUT variable 
 echo "jar-location=app/build/libs/${fname}" >> $GITHUB_OUTPUT
